0b9f182d35f381a706f6e10b980cfbf5b0cc94f9
Toon Schoenmakers <nighteyes1993@gmail.com>
2020-08-14 23:25:16 +0200

    Started working on a single mccompress binary instead

---
+++ src/mccompress.rs
@@ -0,0 +1,95 @@
+extern crate clap;
+extern crate walkdir;
+
+use clap::Clap;
+use flate2::Compression;
+use std::fs::OpenOptions;
+use std::path::PathBuf;
+use walkdir::{DirEntry, WalkDir};
+
+mod region;
+
+#[derive(Clap)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Cleanup(CleanupOpts),
+    Recompress(RecompressOpts),
+}
+
+#[derive(Clap)]
+struct CleanupOpts {
+    // the files/folders that should be processed
+    #[clap(required = true)]
+    input: Vec<PathBuf>,
+}
+
+#[derive(Clap)]
+struct RecompressOpts {
+    // the level of compression that should be used to recompress, 1 being the fastest, 9 being the best
+    #[clap(short, long, default_value = "5")]
+    level: u32,
+
+    // the files/folders that should be processed
+    #[clap(required = true)]
+    input: Vec<PathBuf>,
+}
+
+fn is_mca(entry: &DirEntry) -> bool {
+    let file_type = entry.file_type();
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| file_type.is_dir() || (file_type.is_file() && s.ends_with(".mca")))
+        .unwrap_or(false)
+}
+
+fn cleanup_handle(subopts: &CleanupOpts) {
+    let cleanup = |file: &DirEntry| {
+        println!("{}", file.path().display());
+        let res = || -> Result<usize, region::Error> {
+            let f = OpenOptions::new().write(true).read(true).open(file.path())?;
+            let mut region = region::RegionFile::new(f)?;
+
+            region.clean_junk()
+        };
+
+        match res() {
+            Ok(res) => {
+            },
+            Err(error) => {
+                println!("Error while processing {}", file.path().display());
+            }
+        };
+    };
+
+    for dir in &subopts.input {
+        WalkDir::new(dir)
+            .into_iter()
+            .filter_entry(|e| is_mca(e))
+            .filter_map(|v| v.ok())
+            .for_each(|x| {
+                let metadata = x.metadata().unwrap();
+                if metadata.is_file() && metadata.len() > 0 {
+                    cleanup(&x);
+                }
+            });
+    }
+}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Cleanup(subopts) => {
+            cleanup_handle(&subopts);
+        },
+        SubCommand::Recompress(subopts) => {
+            println!("Level {}", subopts.level);
+        }
+    }
+}
--- src/region.rs
+++ src/region.rs
@@ -1,13 +1,13 @@
-use std::io;
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use flate2;
+use std::io;
 
 #[derive(Debug)]
 pub enum Error {
     Io(io::Error),
-    UnsupportedCompressionFormat{
+    UnsupportedCompressionFormat {
         /// Compression type byte from the format.
-        compression_type: u8
+        compression_type: u8,
     },
 }
 
@@ -22,8 +22,7 @@ impl From<io::Error> for Error {
 /// These normally have a .mca extension on disk.  They contain up to 1024 chunks, each containing
 /// a 32-by-32 column of blocks.
 #[allow(dead_code)]
-pub struct RegionFile<T>
-{
+pub struct RegionFile<T> {
     /// Offsets (in bytes, from the beginning of the file) of each chunk.
     /// An offset of zero means the chunk does not exist
     offsets: Vec<u32>,
@@ -37,9 +36,9 @@ pub struct RegionFile<T>
     cursor: Box<T>,
 }
 
-
-
-impl<R> RegionFile<R> where R: io::Read + io::Seek + io::Write
+impl<R> RegionFile<R>
+where
+    R: io::Read + io::Seek + io::Write,
 {
     /// Parses a region file
     pub fn new(mut r: R) -> Result<RegionFile<R>, Error> {
@@ -56,7 +55,6 @@ impl<R> RegionFile<R> where R: io::Read + io::Seek + io::Write
 
             offsets.push(offset * 4096);
             chunk_size.push(sector_count);
-
         }
 
         for _ in 0..1024 {
@@ -102,9 +100,9 @@ impl<R> RegionFile<R> where R: io::Read + io::Seek + io::Write
     }
 
     /// Returns the amount of chunks in the file are used for this particular ingame chunk
-    /// 
+    ///
     /// # Panics
-    /// 
+    ///
     /// x and z must be between 0 and 31 (inclusive).  If not, panics.
     fn get_chunk_size(&self, x: u8, z: u8) -> usize {
         assert!(x < 32);
@@ -126,9 +124,9 @@ impl<R> RegionFile<R> where R: io::Read + io::Seek + io::Write
     }
 
     /// Figures out how many 'junk' bytes there are present for a specific chunk
-    /// 
+    ///
     /// # Panics
-    /// 
+    ///
     /// x and z must be between 0 and 31 (inclusive).  If not, panics.
     pub fn junk_bytes(&mut self, x: u8, z: u8) -> Result<usize, Error> {
         let offset = self.get_chunk_offset(x, z);
@@ -156,7 +154,12 @@ impl<R> RegionFile<R> where R: io::Read + io::Seek + io::Write
         Ok(0)
     }
 
-    fn recompress_chunk(&mut self, x: u8, z: u8, level: flate2::Compression) -> Result<(usize, usize), Error> {
+    fn recompress_chunk(
+        &mut self,
+        x: u8,
+        z: u8,
+        level: flate2::Compression,
+    ) -> Result<(usize, usize), Error> {
         let offset = self.get_chunk_offset(x, z);
         let chunk_size = self.get_chunk_size(x, z);
 
@@ -167,7 +170,7 @@ impl<R> RegionFile<R> where R: io::Read + io::Seek + io::Write
         assert!(chunk_size > total_len);
 
         if compression_type != 2 {
-            return Err(Error::UnsupportedCompressionFormat{compression_type});
+            return Err(Error::UnsupportedCompressionFormat { compression_type });
         }
 
         let compressed_data = {
@@ -204,12 +207,18 @@ impl<R> RegionFile<R> where R: io::Read + io::Seek + io::Write
         self.cursor.write(&compressed)?;
 
         // we should be at the end of a file chunk now
-        debug_assert_eq!(self.cursor.seek(io::SeekFrom::Current(0)).unwrap() % 4096, 0);
+        debug_assert_eq!(
+            self.cursor.seek(io::SeekFrom::Current(0)).unwrap() % 4096,
+            0
+        );
 
         Ok((total_len, new_len))
     }
 
-    pub fn recompress_region(&mut self, level: flate2::Compression) -> Result<(usize, usize), Error> {
+    pub fn recompress_region(
+        &mut self,
+        level: flate2::Compression,
+    ) -> Result<(usize, usize), Error> {
         let mut out: (usize, usize) = (0, 0);
         for x in 0..32 {
             for z in 0..32 {
@@ -245,7 +254,10 @@ impl<R> RegionFile<R> where R: io::Read + io::Seek + io::Write
         self.cursor.write(&zero)?;
 
         // we should be at the end of a file chunk now
-        debug_assert_eq!(self.cursor.seek(io::SeekFrom::Current(0)).unwrap() % 4096, 0);
+        debug_assert_eq!(
+            self.cursor.seek(io::SeekFrom::Current(0)).unwrap() % 4096,
+            0
+        );
 
         Ok(size)
     }
@@ -263,36 +275,35 @@ impl<R> RegionFile<R> where R: io::Read + io::Seek + io::Write
         Ok(out)
     }
 }
-    /// Loads a chunk into a parsed NBT Tag structure.
-    ///
-    /// # Panics
-    ///
-    /// x and z must be between 0 and 31 (inclusive).  If not, panics.
-    /*pub fn load_chunk(&mut self, x: u8, z: u8) -> Result<nbt::Tag, nbt_error::Error> {
-        let offset = self.get_chunk_offset(x, z); // might panic
+/// Loads a chunk into a parsed NBT Tag structure.
+///
+/// # Panics
+///
+/// x and z must be between 0 and 31 (inclusive).  If not, panics.
+/*pub fn load_chunk(&mut self, x: u8, z: u8) -> Result<nbt::Tag, nbt_error::Error> {
+    let offset = self.get_chunk_offset(x, z); // might panic
 
-        self.cursor.seek(SeekFrom::Start(offset as u64))?;
-        let total_len = self.cursor.read_u32::<BigEndian>()? as usize;
-        let compression_type = self.cursor.read_u8()?;
+    self.cursor.seek(SeekFrom::Start(offset as u64))?;
+    let total_len = self.cursor.read_u32::<BigEndian>()? as usize;
+    let compression_type = self.cursor.read_u8()?;
 
-        if compression_type != 2 {
-            return Err(nbt_error::Error::UnsupportedCompressionFormat{compression_type});
-        }
+    if compression_type != 2 {
+        return Err(nbt_error::Error::UnsupportedCompressionFormat{compression_type});
+    }
 
-        let compressed_data = {
-            let mut v: Vec<u8> = Vec::with_capacity(total_len - 1);
-            v.resize(total_len - 1, 0);
-            self.cursor.read_exact(&mut v)?;
-            v
-        };
+    let compressed_data = {
+        let mut v: Vec<u8> = Vec::with_capacity(total_len - 1);
+        v.resize(total_len - 1, 0);
+        self.cursor.read_exact(&mut v)?;
+        v
+    };
 
-        let mut decoder = flate2::read::ZlibDecoder::new(Cursor::new(compressed_data));
+    let mut decoder = flate2::read::ZlibDecoder::new(Cursor::new(compressed_data));
 
-        let (_, tag) = nbt::Tag::parse(&mut decoder).unwrap();
-        Ok(tag)
+    let (_, tag) = nbt::Tag::parse(&mut decoder).unwrap();
+    Ok(tag)
 
-    }*/
-
+}*/
 
 #[test]
 fn test_region() {
@@ -309,7 +320,6 @@ fn test_region() {
 
     let ts = region.get_chunk_timestamp(14, 10).unwrap();
     assert_eq!(ts, 1383443713);
-
 
     assert!(region.chunk_exists(14, 10));
     assert!(!region.chunk_exists(15, 15));
